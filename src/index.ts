ef4f417d1e56ac1ede0a85db95d61e8690141bba
Harry <hfitz83@gmail.com>
2017-10-19 20:36:04 +0100

    add checks and pre-commit

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,15 @@ const load = () => {
   const lines: string[] = vars.split('\n');
   // assume that the user correctly writes their .env
   lines.forEach(line => {
-    if (line.match(/^\s*([^\n]+)=*([^\n]+)/g)) {
+    // handle comments
+    const isComment: boolean = line.charAt(0) === '#' || line.substring(0, 2) === '//';
+    // check if the line is of the form foo=bar
+    const re: RegExp = new RegExp(/^\s*([^\n]+)=*([^\n]+)/g);
+    const hasCorrectForm: boolean = re.test(line);
+    if (hasCorrectForm || !isComment) {
+      // TODO: parse this better - user may have a url with a '='
       const [key, value] = line.split('=');
+      // set process.env.key to the value - removing any whitespace
       if (value) process.env[key] = value.replace(/\s/, '');
     }
   });
