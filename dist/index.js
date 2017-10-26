296445888c89825a014a77d90ae1fb84fd92eae4
Harry <hfitz83@gmail.com>
2017-10-26 18:36:34 +0100

    use a better form of matching

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var path = require("path");
-var load = function () {
-    var filename = path.join(__dirname, '.env');
-    var vars = fs.readFileSync(filename).toString();
+var re = new RegExp(/^\s*([\w\-\.]+)\s*=\s*([\w\-\.]+)\s*/);
+var load = function (filename) {
+    if (filename === void 0) { filename = '.env'; }
+    // get an absolute link to the filename
+    var absFile = path.join(__dirname, filename);
+    // read the file and cast to string
+    var vars = fs.readFileSync(absFile).toString();
+    // split in to lines of key=value
     var lines = vars.split('\n');
     // assume that the user correctly writes their .env
     lines.forEach(function (line) {
-        if (line.match(/^\s*([^\n]+)=*([^\n]+)/g)) {
-            var _a = line.split('='), key = _a[0], value = _a[1];
-            if (value)
-                process.env[key] = value.replace(/\s/, '');
+        // handle comments
+        var isComment = line.charAt(0) === '#' || line.substring(0, 2) === '//';
+        // check if the line is of the form foo=bar
+        var hasCorrectForm = re.test(line);
+        if (hasCorrectForm || !isComment) {
+            // extract the goods from each
+            var _a = re.exec(line), useless = _a[0], key = _a[1], value = _a[2];
+            // set process.env.key to the value - removing any whitespace
+            if (key && value) {
+                var trimmed = value.replace(/\s/, '');
+                process.env[key] = trimmed;
+            }
         }
     });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const load = () => {
-  const filename: string = path.join(__dirname, '.env');
-  const vars: string = fs.readFileSync(filename).toString();
+const re: RegExp = new RegExp(/^\s*([\w\-\.]+)\s*=\s*([\w\-\.]+)\s*/);
+
+const load = (filename = '.env') => {
+  // get an absolute link to the filename
+  const absFile: string = path.join(__dirname, filename);
+  // read the file and cast to string
+  const vars: string = fs.readFileSync(absFile).toString();
+  // split in to lines of key=value
   const lines: string[] = vars.split('\n');
   // assume that the user correctly writes their .env
   lines.forEach(line => {
     // handle comments
     const isComment: boolean = line.charAt(0) === '#' || line.substring(0, 2) === '//';
     // check if the line is of the form foo=bar
-    const re: RegExp = new RegExp(/^\s*([^\n]+)=*([^\n]+)/g);
     const hasCorrectForm: boolean = re.test(line);
     if (hasCorrectForm || !isComment) {
-      // TODO: parse this better - user may have a url with a '='
-      const [key, value] = line.split('=');
+      // extract the goods from each
+      const [useless, key, value] = re.exec(line);
       // set process.env.key to the value - removing any whitespace
-      if (value) process.env[key] = value.replace(/\s/, '');
+      if (key && value) {
+        const trimmed: string = value.replace(/\s/, '');
+        process.env[key] = trimmed;
+      }
     }
   });
 };
